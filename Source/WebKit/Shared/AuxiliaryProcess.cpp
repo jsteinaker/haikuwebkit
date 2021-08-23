745aa3b8a09a0769ef612f4e3e7ce646320ade06
Adrien Destugues <pulkomandy@pulkomandy.tk>
2021-08-23 22:24:50 +0200

    AuxilliaryProcess: fix build

--- Source/WebKit/Shared/AuxiliaryProcess.cpp
+++ Source/WebKit/Shared/AuxiliaryProcess.cpp
@@ -33,6 +33,7 @@
 #include "SandboxInitializationParameters.h"
 #include "WebPageProxyIdentifier.h"
 #include <WebCore/LogInitialization.h>
+#include <WebCore/NotImplemented.h>
 #include <pal/SessionID.h>
 #include <wtf/LogInitialization.h>
 #include <wtf/SetForScope.h>
