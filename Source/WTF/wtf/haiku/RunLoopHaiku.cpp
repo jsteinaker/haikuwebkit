88c66a6d3344c14c89511968167a7fea40357121
Adrien Destugues <pulkomandy@gmail.com>
2021-12-10 18:01:04 +0100

    RunLoopHaiku: add missing method stub

--- Source/WTF/wtf/haiku/RunLoopHaiku.cpp
+++ Source/WTF/wtf/haiku/RunLoopHaiku.cpp
@@ -169,4 +169,9 @@ RunLoop::CycleResult RunLoop::cycle(RunLoopMode)
     return CycleResult::Continue;
 }
 
+Seconds RunLoop::TimerBase::secondsUntilFire() const
+{
+    // FIXME implement
+    return 0_s;
+}
 }
